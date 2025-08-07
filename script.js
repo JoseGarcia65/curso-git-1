e65b3921c94707ff438d4da713f89229eae3832a
JoseGarcia65 <96267034+JoseGarcia65@users.noreply.github.com>
2025-08-08 00:31:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<iframe src="https://fred.stlouisfed.org/graph/graph-landing.php?g=1Lc8Z&width=470&height=300" scrolling="yes" frameborder="0" style="overflow:hidden; width:670px; height:525px;" allowTransparency="true" loading="lazy"></iframe>
+<iframe src="https://fred.stlouisfed.org/graph/graph-landing.php?g=1LcbT&width=670&height=475" scrolling="no" frameborder="0" style="overflow:hidden; width:670px; height:525px;" allowTransparency="true" loading="lazy"></iframe>
 
 const resultado = document.getElementById('resultado');
 const ctx = document.getElementById('grafico').getContext('2d');
@@ -71,3 +71,4 @@ obtenerPrimaRiesgo();
 
 // Actualizar cada hora
 setInterval(obtenerPrimaRiesgo, 3600000);
+
